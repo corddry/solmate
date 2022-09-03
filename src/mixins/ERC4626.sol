1c6bf3b536a7240299a784da5cfdc744025ff8e8
Jack Corddry <jackcorddry@gmail.com>
2022-09-03 16:36:11 -0700

    replace solmate ERC20 with Openzeppelin ERC20

--- src/mixins/ERC4626.sol
+++ src/mixins/ERC4626.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import {ERC20} from "../tokens/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {SafeTransferLib} from "../utils/SafeTransferLib.sol";
 import {FixedPointMathLib} from "../utils/FixedPointMathLib.sol";
 
